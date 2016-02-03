771284602f644e6a347f9388d7e6e6e9bdc54d58
soh <tempe31@gmail.com>
2016-02-03 14:47:43 +0100

    Names on exerises

--- Source/2 Advanced Programming/Week1/Exercises.scala
+++ Source/2 Advanced Programming/Week1/Exercises.scala
@@ -1,7 +1,7 @@
 // Advanced Programming, Exercises by A. Wąsowski, IT University of Copenhagen
 //
-// AUTHOR1: Søren Harrison
-// AUTHOR2: Jan Vium Enghoff
+// AUTHOR1: Jan Vium Enghoff (jaen@itu.dk) 
+// AUTHOR2: Søren Harrison (soeh@itu.dk)
 //
 // Write names and ITU email addresses of both group members that contributed to
 // the solution of the exercise (in alphabetical order by family name).
