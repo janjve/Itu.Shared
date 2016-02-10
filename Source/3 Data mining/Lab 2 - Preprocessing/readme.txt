6a37a894da29e7f8cace9d0cfe3bc53985894eae
Jan Vium Enghoff <jan.jve@gmail.com>
2016-02-10 14:31:33 +0100

    Datamining: week 2 added project structure and exercise files.

---
+++ Source/3 Data mining/Lab 2 - Preprocessing/readme.txt
@@ -0,0 +1,23 @@
+Info on the different data files
+
+DataMining 2016 (Responses).csv
+CSV file containing data from questionnaire given to 2016 Data Mining students (you!). 
+"-" have been inserted when there is no value for an attribute.
+47 responses in total.
+
+data_mining_2014_dataset:
+CSV file containing the data from the questionnaires filled out by the 2014 Data Mining students. 
+"-" have been inserted when there is no value for an attribute.
+
+Data_Mining_Student_DataSet_2013:
+Original CSV file containing the data from the questionnaires filled out by the 2013 Data Mining students.
+Empty string is used when there is no value for an attribute.
+Some issues with this file:
+Row 16 is invalid, due to a missing attribute ("noor_hometown" or "therb_fortt_glag".).
+Row 35 is invalid, due to two missing attributes (english_level and fav_colour)
+
+Data_Mining_Student_DataSet_2013_Fixed:
+CSV file containing the data from the questionnaires filled out by the 2013 Data Mining students.
+This file has been corrected for the above mentioned issues.
+Many thanks to Niels Abildgaard for pointing out these issues and for helping correct them.
+"-" have been inserted when there is no value for an attribute.
---
+++ Source/3 Data mining/Lab 2 - Preprocessing/src/CSVFileReader.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * The CSVFileReader class is used to load a csv file
+ * @author andershh and hjab
+ *
+ */
+public class CSVFileReader {
+	/**
+	 * The read method reads in a csv file as a two dimensional string array.
+	 * This method is utilizes the string.split method for splitting each line of the data file.
+	 * String tokenizer bug fix provided by Martin Marcher.
+	 * @param csvFile File to load
+	 * @param seperationChar Character used to seperate entries
+	 * @param nullValue What to insert in case of missing values
+	 * @return Data file content as a 2D string array
+	 * @throws IOException
+	 */
+	public static String[][] readDataFile(String csvFile, String seperationChar, String nullValue, boolean skipHeaderRow) throws IOException
+	{
+		
+		List<String[]> lines = new ArrayList<String[]>();
+		BufferedReader bufRdr = new BufferedReader(new FileReader(new File(csvFile)));
+		
+//		// read the header
+//		String line = bufRdr.readLine();
+		String line;
+		
+		while ((line = bufRdr.readLine()) != null) {
+			
+			if (skipHeaderRow) {
+				skipHeaderRow = false;
+				continue;
+			}
+			
+			String[] arr = line.split(seperationChar); 
+			
+			for(int i = 0; i < arr.length; i++)
+			{
+				if(arr[i].equals(""))
+				{
+					arr[i] = nullValue;
+				}				
+			}
+			
+			lines.add(arr);			
+		}
+		
+		String[][] ret = new String[lines.size()][];
+		bufRdr.close();
+		return lines.toArray(ret);
+	}
+	
+	public static void main(String args[]) {
+		boolean skipHeaderRow = false;
+		
+		try {
+			String[][] data = readDataFile("DataMining 2016 (Responses).csv",";", "-",skipHeaderRow  );
+			for (String[] line : data) {
+				System.out.println(Arrays.toString(line));
+			}
+			System.out.println("Number of tuples loaded: "+data.length);
+			if (!skipHeaderRow)
+				System.out.println("(First row is data lables)");
+		} catch (IOException e) {
+			System.err.println(e.getLocalizedMessage());
+		}
+	}
+}
