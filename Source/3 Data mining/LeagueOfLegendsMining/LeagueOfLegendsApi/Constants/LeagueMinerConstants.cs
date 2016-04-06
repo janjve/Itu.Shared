16cbfe7ee215ca01e39b8d8ba14b22053ab46924
soh <tempe31@gmail.com>
2016-04-06 15:57:20 +0200

    Data mining - Lol

---
+++ Source/3 Data mining/LeagueOfLegendsMining/LeagueOfLegendsApi/Constants/LeagueMinerConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueOfLegendsApi.Constants
+{
+    public static class LeagueMinerConstants
+    {
+        public static string API_KEY { get { return "27e3d00f-586c-4242-9a94-af2d082d04ea"; } }
+        public static string LoLEndpoint { get { return "https://euw.api.pvp.net/"; } }
+        public static string MatchListEndpoint { get { return "api/lol/{0}/v2.2/matchlist/by-summoner/{1}"; } }
+        public static string MatchEndpoint { get { "/api/lol/{0}/v2.2/match/{1}"; } }
+        public static string LeagueEndpoint { get { "/api/lol/{0}/v2.5/league/by-summoner/{1}"; } }
+    }
+}
---
+++ Source/3 Data mining/LeagueOfLegendsMining/LeagueOfLegendsApi/LeagueMiner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueOfLegendsApi.Constants;
+namespace LeagueOfLegendsApi
+{
+    public class LeagueMiner
+    {
+        public List<int> GetMatchList(int summonerId)
+        {
+            string url = String.Format(LeagueMinerConstants.LoLEndpoint + MatchListEndpoint, LeagueMinerConstants.); 
+            var webRequest = new WebRequest.Create( + );
+        }
+    }
+}
