f2d54d8a502f6a04a72612d5ae5320f0843caf60
Jan Vium Enghoff <jan.jve@gmail.com>
2016-04-07 16:14:20 +0200

    Intelligent system project

--- Source/4 Intelligent programming/project2/Queens/BDDExamples.java
+++ Source/4 Intelligent programming/project2/Queens/BDDExamples.java
@@ -1,6 +1,8 @@
 
 import net.sf.javabdd.*;
 
+import java.util.List;
+
 public class BDDExamples {
 
 	/**
@@ -8,40 +10,43 @@ public class BDDExamples {
 	 */
 	public static void main(String[] args){
 		BDDFactory fact = JFactory.init(10,10);
-		fact.setVarNum(2);
-		
+		fact.setVarNum(3);
+
 		BDD True = fact.one();
 		BDD False = fact.zero();
 		
 		// the expression x0
 		BDD x_0 = fact.ithVar(0);
-		
+		BDD x_2 = fact.nithVar(0);
+
 		//the expression not x1
 		BDD nx_1 = fact.nithVar(1);
-		
+		BDD nx_2 = fact.nithVar(2);
+
 		// the expression (not x1 or x0) and (True or false)
-		BDD b = nx_1.or(x_0).and(True.or(False));
-		
+		BDD b = nx_1.and(x_0).and(True.or(False));
+
 		// Checks whether or not expression is unsat
 		System.out.println("b is unsat? : " + b.isZero());
 		
 		// checks whether expression is tautology
 		System.out.println("b is tautology? : " + b.isOne());
-		
+
 		// In order to restrict or quantify the expression to a given assignment
 		// we give the assignment as a conjunction where positive variables
 		// indicate that the variable should be restricted to false, and vice versa.
-		BDD restriction_x1_true_x0_false = fact.ithVar(1).and(fact.nithVar(0)); 
+		BDD restriction_x1_true_x0_false = fact.ithVar(1).and(fact.ithVar(0));
 		
 		BDD restricted = b.restrict(restriction_x1_true_x0_false);
+
 		BDD existed = b.exist(x_0);
-		
+
 		// Exist. should be tautology
 		System.out.println("Existiential quant. cause taut: " + existed.isOne());
-		
+
 		// Restriction shoule be unsat:
 		System.out.println("Restriction caused unsat: " + restricted.isZero());
-		
+
 		// how to perform replacement
 		BDDPairing replacement = fact.makePair();
 		int[] from = {1};
--- Source/4 Intelligent programming/project2/Queens/QueensLogic.java
+++ Source/4 Intelligent programming/project2/Queens/QueensLogic.java
@@ -1,45 +1,139 @@
 /**
  * This class implements the logic behind the BDD for the n-queens problem
  * You should implement all the missing methods
- * 
- * @author Stavros Amanatidis
  *
+ * @author Stavros Amanatidis
  */
+
 import java.util.*;
 
 import net.sf.javabdd.*;
 
-public class QueensLogic {
-    private int x = 0;
-    private int y = 0;
+public class QueensLogic
+{
+    private int sizeX = 0;
+    private int sizeY = 0;
     private int[][] board;
+    private final int INVALID = -1;
+    private final int QUEEN = 1;
+    private final int EMPTY = 0;
 
+    private BDDFactory factory;
+    private BDD nQueenBdd;
+    private BDD queen;
 
-    public QueensLogic() {
-       //constructor
+    public QueensLogic()
+    {
+
     }
 
-    public void initializeGame(int size) {
-        this.x = size;
-        this.y = size;
-        this.board = new int[x][y];
+    public void initializeGame(int size)
+    {
+        this.sizeX = size;
+        this.sizeY = size;
+        this.board = new int[sizeX][sizeY];
+
+        factory = JFactory.init(2000000, 200000);
+        factory.setVarNum(sizeX * sizeY);
+        initBDD();
     }
 
-   
-    public int[][] getGameBoard() {
+
+    public int[][] getGameBoard()
+    {
         return board;
     }
 
-    public boolean insertQueen(int column, int row) {
+    public boolean insertQueen(int column, int row)
+    {
 
-        if (board[column][row] == -1 || board[column][row] == 1) {
+        if (board[column][row] == -1 || board[column][row] == 1)
+        {
             return true;
         }
-        
-        board[column][row] = 1;
-        
+
+        board[column][row] = QUEEN;
+
         // put some logic here..
-      
+        restrictBDD(column, row);
+        updateBoard();
+
         return true;
     }
+
+    private void restrictBDD(int col, int row)
+    {
+        BDD restrictQueen = factory.nithVar(col*sizeX+row);
+        nQueenBdd = nQueenBdd.restrict(restrictQueen);
+    }
+
+    private void updateBoard()
+    {
+        List satisfyingList = nQueenBdd.allsat();
+
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++){
+                /*for(s : satisfyingList){
+                    satisfyingList.get(x*sizeX+y);
+                }*/
+            }
+        }
+    }
+
+    private void initBDD()
+    {
+        BDD[][] variables = new BDD[sizeX][sizeY];
+
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++)
+            {
+                variables[x][y] = factory.ithVar(x * sizeX + y);
+            }
+        }
+
+        nQueenBdd = variables[0][0].or(factory.nithVar(0+1).and(factory.nithVar(0+2)));
+/*
+        if (false)
+        {
+            // Horizontal rules
+            BDD horizontal = null;
+            for (int x = 0; x < sizeX; x++)
+            {
+                BDD col = null;
+                for (int y = 0; y < sizeY; y++)
+                {
+                    BDD cell = null;
+                    for (int c = 0; c < sizeY; c++)
+                    {
+                        if (cell == null)
+                            cell = c == y ? variables[x][c] : variables[x][c]NEGATE;
+                        else
+                            cell = c == y ? cell.and(variables[x][c]) : cell.and(variables[x][c] NEGATE);
+                    }
+                    if (col == null)
+                        col = cell;
+                    else
+                        col = col.or(cell);
+                }
+                if (horizontal == null)
+                    horizontal = col;
+                else horizontal = horizontal.and(col);
+            }
+            nQueenBdd = horizontal;
+
+            BDD vertical = null;
+            nQueenBdd = nQueenBdd.and(horizontal);
+
+            BDD diagonallyAscend = null;
+            nQueenBdd = nQueenBdd.and(diagonallyAscend);
+
+            BDD diagonallyDescend = null;
+            nQueenBdd = nQueenBdd.and(diagonallyDescend);
+        }*/
+    }
+
+
+
 }
