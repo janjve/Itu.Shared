374d70aa22e2101a8b6c3fe69f3067a52c7573c6
unknown <tempe31@gmail.com>
2016-04-08 16:05:57 +0200

    Advanced programming miniproject

--- Source/2 Advanced Programming/Miniproject1/090-miniproject-1/src/test/scala/fpinscala/laziness/StreamSpecSoehJaen.scala
+++ Source/2 Advanced Programming/Miniproject1/090-miniproject-1/src/test/scala/fpinscala/laziness/StreamSpecSoehJaen.scala
@@ -1,11 +1,15 @@
 // Advanced Programming
 // Andrzej Wasowski, IT University of Copenhagen
+// Name1: Søren Harrison
+// Name2: Jan Vium Enghoff
+// Group: 23
 
 package fpinscala.laziness
 import scala.language.higherKinds
 
 import org.scalatest.FlatSpec
 import org.scalatest.prop.Checkers
+import org.scalatest.exceptions._
 import org.scalacheck._
 import org.scalacheck.Prop._
 import Arbitrary.arbitrary
@@ -16,10 +20,10 @@ import Arbitrary.arbitrary
 // fail on them :)
 
 import stream00._    // uncomment to test the book solution
-// import stream01._ // uncomment to test the broken headOption implementation
-// import stream02._ // uncomment to test another version that breaks headOption
+//import stream01._ // uncomment to test the broken headOption implementation
+//import stream02._ // uncomment to test another version that breaks headOption
 
-class StreamSpecWasowski extends FlatSpec with Checkers {
+class StreamSpecSoehJaen extends FlatSpec with Checkers {
 
   import Stream._
 
@@ -34,12 +38,22 @@ class StreamSpecWasowski extends FlatSpec with Checkers {
   // An example generator of random finite non-empty streams
   def list2stream[A] (la :List[A]): Stream[A] = la.foldRight (empty[A]) (cons[A](_,_))
 
+
+
   // In ScalaTest we use the check method to switch to ScalaCheck's internal DSL
   def genNonEmptyStream[A] (implicit arbA :Arbitrary[A]) :Gen[Stream[A]] = {
     for { la <- arbitrary[List[A]] suchThat (_.nonEmpty)}
     yield list2stream (la)
   }
 
+  def genInfiniteStream[A] (implicit arbA :Arbitrary[A]) :Gen[Stream[A]] = {
+    for { la <- arbitrary[A] }
+    yield constantStream(la)
+  }
+
+  def constantStream[A] (n :A) :Stream[A] = {
+    Cons(() => n, () => constantStream(n))
+  }
 
   // a property test:
 
@@ -57,12 +71,11 @@ class StreamSpecWasowski extends FlatSpec with Checkers {
     implicit def arbIntStreamCallback = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
     ("random" |:
       Prop.forAll { (n: Stream[Int]) =>{  
-        var evaluated = false;
         cons(n.take(1), n.drop(1).map(x => {
-          evaluated = true
+          throw new TestFailedException("evaluated the tail",1)
           x
         })).headOption
-        !evaluated
+        true
       }
       })
   }
@@ -74,12 +87,11 @@ class StreamSpecWasowski extends FlatSpec with Checkers {
     implicit def arbIntStreamCallback = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
     ("random" |:
       Prop.forAll { (n: Int, s: Stream[Int]) =>{  
-        var evaluated = false;
         s.map(x => {
-          evaluated = true
+          throw new TestFailedException("forced a head somewhere",1)
           x
         }).take(n)
-        !evaluated
+        true
       }
       })
   }
@@ -88,13 +100,12 @@ class StreamSpecWasowski extends FlatSpec with Checkers {
     implicit def arbIntStreamCallback = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
     ("random" |:
       Prop.forAll { (n: Int, s: Stream[Int]) =>{  
-        var evaluated = false;
         var j = n;
-        s.takeWhile_1(x => { j = j-1; j >= 0 }).append(s.drop(n).map(x => {
-          evaluated = true
+        s.takeWhile(x => { j = j-1; j >= 0 }).append(s.drop(n).map(x => {
+          throw new TestFailedException("forced n+1st head",1)
           x
         })).take(n).toList
-        !evaluated
+        true
       }
       })
   }
@@ -115,27 +126,103 @@ class StreamSpecWasowski extends FlatSpec with Checkers {
 
   it should "s.drop(n).drop(m) == s.drop(n+m) for any ints n and m" in check {
     implicit def arbIntStreamCallback = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    implicit def arbInt = Arbitrary(Gen.chooseNum(0,Int.MaxValue/2))
     ("random" |:
-      Prop.forAll { (n: Int, m: Int, s: Stream[Int]) =>{
-        var n1 = Math.abs(n)
-        var m1 = Math.abs(m)
-        val sum = n1+m1
-        println(s"$n1 $m1 $sum")
-        if(n1+m1 < 0) {
-          n1 = n1/2
-          m1 = m1/2
-        }
-        val s1 = s.drop(n1).drop(m1).toList
-        val s2 = s.drop(n1+m1).toList
+      Prop.forAll { (n: Int, m: Int, s: Stream[Int]) => {
+        val s1 = s.drop(n).drop(m).toList
+        val s2 = s.drop(n+m).toList
         s1 == s2
       }
       })
   }
 
+  it should "s.drop(n) does not force any of the dropped elements heads" in check {
+    implicit def arbIntStreamCallback = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    implicit def arbInt = Arbitrary(Gen.chooseNum(0,Int.MaxValue))
+     ("empty tail" |:
+      Prop.forAll { (n: Int, s: Stream[Int]) => {
+        s.take(n).map(x => {
+          throw new TestFailedException("forced a head somewhere",1)
+          x
+          }).drop(n).headOption
+        true
+      }
+      })
+  }
 
+  it should "s.drop(n) does not force any of the dropped elements heads even when forcing tail" in {
+    def callback(n: Int): Int = {
+      assert(false)
+      n
+    }
+    cons(callback(3), cons(callback(4), cons(5, empty))).drop(2).headOption
+    assert(true)
+  }
+
+  behavior of "map"
+
+  it should "x.map(id) == x (where id is the identity function)" in check {
+    implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    ("random" |:
+      Prop.forAll { (n: Int, s: Stream[Int]) => {
+        val s1 = s.map(x => x)
+        s.startsWith(s1) && s.toList.length == s1.toList.length
+      }
+      })
+  }
+
+  it should "map terminates on infinite streams" in check {
+    implicit def arbInfIntStream = Arbitrary[Stream[Int]] (genInfiniteStream[Int])
+    ("random" |:
+      Prop.forAll { (s: Stream[Int]) => {
+        s.map(x => x+1)
+        true
+      }
+      })
+  }
+
+  behavior of "append"
+
+  it should "return the same stream when appending an empty stream" in check {
+    implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    ("random" |:
+      Prop.forAll { (s: Stream[Int]) => {
+        val s1 = s.append(empty)
+        s.startsWith(s1) && s.toList.length == s1.toList.length
+      }
+      })
+  }
+
+  it should "hold that s.append(s2.append(s3)) == s.append(s2).append(s3)" in check {
+    implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    ("random" |:
+      Prop.forAll { (s1: Stream[Int], s2: Stream[Int], s3: Stream[Int]) => {
+        val s4 = s1.append(s2).append(s3) 
+        val s5 = s1.append(s2.append(s3))
+        s4.startsWith(s5) && s4.toList.length == s5.toList.length
+      }
+      })
+  }
+
+  it should "not change the size no matter what order append is called" in check {
+    implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    ("random" |:
+      Prop.forAll { (s1: Stream[Int], s2: Stream[Int]) => {
+        val s3 = s1.append(s2)
+        val s4 = s2.append(s1)
+        s3.toList.length == s4.toList.length
+      }
+      })
+  }
+
+  it should "hold that s1 comes before s2 for s1.append(s2)." in check {
+  implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+  ("random" |:
+    Prop.forAll { (s1: Stream[Int], s2: Stream[Int]) => {
+      val s3 = s1.append(s2)
+      s3.startsWith(s1) && s3.drop(s1.toList.length).startsWith(s2) 
+    }
+    })
+  }
 
 }
-// Cons(() => A, () => Stream[A])
-// h()
-// Cons(A, => Stream[A])
-// if(t istype(stream)) false else true
