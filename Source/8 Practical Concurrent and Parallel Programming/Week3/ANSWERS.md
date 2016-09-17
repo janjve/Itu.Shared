17b7fb129d418dbc72e41c24c1be992409c083be
Jan Vium Enghoff <jan.jve@gmail.com>
2016-09-17 10:05:00 +0200

    Finished Exercise 3 in PCPP.

---
+++ Source/8 Practical Concurrent and Parallel Programming/Week3/ANSWERS.md
@@ -0,0 +1,18 @@
+Author 1: Jan Vium Enghoff <jaen@itu.dk>
+Author 2: Jan Vium Enghoff <soeh@itu.dk>
+
+Week 3
+======
+Exercise 1
+----------
+
+Exercise 2
+----------
+
+Exercise 3
+----------
+3.12
+- Slapping .parallel() on the stream will make the query run much slower than the query from 3.11. 
+
+Exercise 4
+----------
--- Source/8 Practical Concurrent and Parallel Programming/Week3/src/TestWordStream.java
+++ Source/8 Practical Concurrent and Parallel Programming/Week3/src/TestWordStream.java
@@ -6,19 +6,19 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.IntSummaryStatistics;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeMap;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class TestWordStream {
-    public static void main(String[] args) {
-        String filename = "C:\\Code\\Itu.Shared\\Source\\8 Practical Concurrent and Parallel Programming\\Week3\\src\\words";
+public class TestWordStream
+{
+    public static void main(String[] args)
+    {
+        String filename = "D:\\Development\\Itu.Shared\\Source\\8 Practical Concurrent and Parallel Programming\\Week3\\src\\words";
         //Exercise 3.1
         Stream<String> stream = readWords(filename);
         //System.out.println(stream.count());
@@ -50,35 +50,64 @@ public class TestWordStream {
         //stream4.forEach((k,v) -> v.forEach(System.out::println));
 
         //Exercise 3.9
-        Stream<Map<Character, Integer>> stream5 = stream.map(TestWordStream::letters);//.limit(100);
-        //stream5.forEach((x) -> x.forEach((k,v) -> System.out.println(k + "  " + v)));
+        Stream<Map<Character, Integer>> stream5 = stream.map(TestWordStream::letters);
+        //stream5.limit(100).forEach((x) -> x.forEach((k,v) -> System.out.println(k + "  " + v)));
 
         //Exercise 3.10
-        Integer res = stream5.map(x -> x.containsKey('e') ? x.get('e') : 0).reduce(0, (a, z) -> a + z);
-        System.out.println(res);
+        //Integer res = stream5.map(x -> x.containsKey('e') ? x.get('e') : 0).reduce(0, (a, z) -> a + z);
+        //System.out.println(res);
 
+        //Exercise 3.11
+        /*
+        long count = stream5
+                .collect(Collectors.groupingBy(x -> x))
+                .values()
+                .stream()
+                .filter(x -> x.size() > 1)
+                .count();
+
+        System.out.println(count);
+        */
+
+        //Exercise 3.12
+        long countInParallel = stream5
+                .parallel()
+                .collect(Collectors.groupingBy(x -> x))
+                .values()
+                .stream()
+                .filter(x -> x.size() > 1)
+                .count();
+        System.out.println(countInParallel);
     }
 
 
-    public static Stream<String> readWords(String filename) {
-        try {
+    public static Stream<String> readWords(String filename)
+    {
+        try
+        {
             return Files.lines(Paths.get(filename));
-        } catch (IOException exn) {
+        } catch (IOException exn)
+        {
+            System.out.println("File not found.");
             return Stream.<String>empty();
         }
     }
 
-    public static boolean isPalindrome(String s) {
-        for(int i = 0; i < s.length()/2; i++) {
-            if(s.charAt(i) != s.charAt(s.length()-i-1))
+    public static boolean isPalindrome(String s)
+    {
+        for (int i = 0; i < s.length() / 2; i++)
+        {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1))
                 return false;
         }
         return true;
     }
 
-    public static Map<Character, Integer> letters(String s) {
+    public static Map<Character, Integer> letters(String s)
+    {
         Map<Character, Integer> res = new TreeMap<>();
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++)
+        {
             char key = Character.toLowerCase(s.charAt(i));
             res.put(key, res.containsKey(key) ? res.get(key) + 1 : 1);
         }
@@ -86,9 +115,12 @@ public class TestWordStream {
     }
 }
 
-class WordSummary implements Consumer<String>{
+class WordSummary implements Consumer<String>
+{
     public int min, max, sum, count;
-    public WordSummary() {
+
+    public WordSummary()
+    {
         min = Integer.MAX_VALUE;
         max = Integer.MIN_VALUE;
         sum = 0;
@@ -96,17 +128,19 @@ class WordSummary implements Consumer<String>{
     }
 
     @Override
-    public void accept(String s) {
+    public void accept(String s)
+    {
         int length = s.length();
-        min = Math.min(min,length);
-        max = Math.max(max,length);
+        min = Math.min(min, length);
+        max = Math.max(max, length);
         sum += length;
-        count ++;
+        count++;
     }
 
-    public void combine(WordSummary a) {
-        min = Math.min(min,a.min);
-        max = Math.max(max,a.max);
+    public void combine(WordSummary a)
+    {
+        min = Math.min(min, a.min);
+        max = Math.max(max, a.max);
         sum += a.sum;
         count += a.count;
     }
