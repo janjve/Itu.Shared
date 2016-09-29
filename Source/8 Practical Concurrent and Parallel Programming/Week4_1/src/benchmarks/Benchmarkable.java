d11ce59ac547c5949fc30d6fcc6cbc107ec2bc32
unknown <tempe31@gmail.com>
2016-09-29 12:34:43 +0200

    PCPP week4 cleanup

--- Source/8 Practical Concurrent and Parallel Programming/Week4_1/src/benchmarks/Benchmarkable.java
+++
@@ -1,6 +0,0 @@
-import java.util.function.IntToDoubleFunction;
-
-public abstract class Benchmarkable implements IntToDoubleFunction {
-  public void setup() { }
-  public abstract double applyAsDouble(int i);
-}
