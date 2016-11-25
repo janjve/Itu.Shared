a51457f2a65dd7ed841c2e1e33be2775ebbe04b8
Jan Vium Enghoff <jan.jve@gmail.com>
2016-11-25 10:35:17 +0100

    Week 12

---
+++ Source/8 Practical Concurrent and Parallel Programming/Week12/src/ABCTest.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import akka.actor.*;
+
+public class ABCTest {
+	public static void main(String[] args){
+		 /*final ActorSystem system = ActorSystem.create("HelloWorldSystem");
+		 final ActorRef myactor =
+		 system.actorOf(Props.create(MyActor.class), "myactor");
+		 myactor.tell(new MyMessage("hello"), ActorRef.noSender());
+		 myactor.tell(new MyMessage("world"), ActorRef.noSender());
+		 try {
+		 System.out.println("Press return to terminate...");
+		 System.in.read();
+		 } catch(IOException e) {
+		 e.printStackTrace();
+		 } finally {
+		 system.shutdown();
+		 }*/
+	}
+}
+/*
+// -- MESSAGE --------------------------------------------------
+class MyMessage implements Serializable { // must be Serializable:
+ public final String s;
+ public MyMessage(String s) { this.s = s; }
+}
+// -- ACTOR --------------------------------------------------
+
+class MyActor extends UntypedActor {
+// can have (local) state
+ private int count = 0; 
+ public void onReceive(Object o) throws Exception { // reacting to message:
+if (o instanceof MyMessage) {
+ MyMessage message = (MyMessage) o;
+ System.out.println(message.s + " (" + count + ")");
+ count++;
+}
+ }
+} */
+
